32a24e27e8e1c637bfa3795d4012c12a8c49a7b5
Michelle Poterek <poterekm@gmail.com>
2017-07-21 11:29:30 -0700

    fix shoe tests to work again

--- src/Shoe.php
+++ src/Shoe.php
@@ -4,39 +4,32 @@ class Shoe
     private $brand;
     private $price;
     private $id;
-
     function __construct($brand, $price, $id = null)
     {
         $this->brand = $brand;
         $this->price = $price;
         $this->id = $id;
     }
-
     function getBrand()
     {
         return $this->brand;
     }
-
     function setBrand($new_brand)
     {
         $this->brand = (string) $new_brand;
     }
-
     function getPrice()
     {
         return $this->price;
     }
-
     function setPrice($new_price)
     {
         $this->price = (string) $new_price;
     }
-
     function getId()
     {
         return $this->id;
     }
-
     function save()
     {
         $executed = $GLOBALS['DB']->exec("INSERT INTO shoes (brand, price) VALUES ('{$this->getBrand()}', '{$this->getPrice()}')");
@@ -47,7 +40,6 @@ class Shoe
             return false;
         }
     }
-
     static function getAll()
     {
         $returned_shoes = $GLOBALS['DB']->query("SELECT * FROM shoes;");
@@ -61,7 +53,6 @@ class Shoe
         }
         return $shoes;
     }
-
     static function deleteAll()
     {
         $executed = $GLOBALS['DB']->exec("DELETE FROM shoes;");
@@ -71,7 +62,6 @@ class Shoe
             return false;
         }
     }
-
     static function find($search_id)
     {
         $found_shoe = null;
@@ -88,7 +78,6 @@ class Shoe
         }
         return $found_shoe;
     }
-
     function update($new_brand)
     {
         $executed = $GLOBALS['DB']->exec("UPDATE shoes SET brand = '{$new_brand}' WHERE id = {$this->getId()};");
@@ -99,31 +88,29 @@ class Shoe
             return false;
         }
     }
-
-    // function addStore($store)
-    // {
-    //     $executed = $GLOBALS['DB']->exec("INSERT INTO shoes_stores (shoe_id, store_id) VALUES ({$this->getId()}, {$store->getId()});");
-    //     if ($executed) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-    //
-    // function getStores()
-    // {
-    //     $returned_stores = $GLOBALS['DB']->query("SELECT stores.* FROM shoes JOIN shoes_stores ON (shoes_stores.shoes_id = shoes.id) JOIN students ON (stores.id = shoes_stores.store_id) WHERE store.id = {$this->getId()};");
-    //     $stores = array();
-    //     foreach($returned_stores as $store) {
-    //         $store = $store['store'];
-    //         $id = $store['id'];
-    //         $new_store = new Store($store, $id);
-    //         array_push($stores, $new_store);
-    //     }
-    //     return $stores;
-    // }
-
-
-
 }
+// function addStore($store)
+// {
+//     $executed = $GLOBALS['DB']->exec("INSERT INTO shoes_stores (shoe_id, store_id) VALUES ({$this->getId()}, {$store->getId()});");
+//     if ($executed) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// function getStores()
+// {
+//     $returned_stores = $GLOBALS['DB']->query("SELECT stores.* FROM shoes JOIN shoes_stores ON (shoes_stores.shoes_id = shoes.id) JOIN students ON (stores.id = shoes_stores.store_id) WHERE store.id = {$this->getId()};");
+//     $stores = array();
+//     foreach($returned_stores as $store) {
+//         $store = $store['store'];
+//         $id = $store['id'];
+//         $new_store = new Store($store, $id);
+//         array_push($stores, $new_store);
+//     }
+//     return $stores;
+// }
+// }
+
 ?>
--- tests/ShoeTest.php
+++ tests/ShoeTest.php
@@ -1,19 +1,16 @@
 <?php
-
     /**
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
     */
 
     require_once "src/Shoe.php";
-    require_once "src/Store.php";
-
+    // require_once "src/Store.php";
 
     $server = 'mysql:host=localhost:8889;dbname=shoe_store_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
-
 
     class ShoeTest extends PHPUnit_Framework_TestCase
     {
@@ -21,82 +18,63 @@
         {
             Shoe::deleteAll();
         }
-
         function testGetBrand()
         {
             //Arrange
             $brand = "Adidas";
             $price = "50";
             $test_shoe = new Shoe($brand, $price);
-
             //Act
             $result = $test_shoe->getBrand();
-
             //Assert
             $this->assertEquals($brand, $result);
         }
-
         function testSetBrand()
         {
             $brand = "Saucany";
             $price = "35";
             $test_shoe = new Shoe($brand, $price);
             $new_brand = "Paws";
-
             $test_shoe->setBrand($new_brand);
             $result = $test_shoe->getBrand();
-
             $this->assertEquals($new_brand, $result);
         }
-
         function testGetPrice()
         {
             $brand = "Nike";
             $price = "666";
             $test_shoe = new Shoe($brand, $price);
-
             $result = $test_shoe->getPrice();
-
             $this->assertEquals($price, $result);
         }
-
         function testSetNumber()
         {
             $brand = "Bans";
             $price = "39";
             $test_shoe = new Shoe($brand, $price);
             $new_price = "Jo Boi Day";
-
             $test_shoe->setPrice($new_price);
             $result = $test_shoe->getPrice();
-
             $this->assertEquals($new_price, $result);
         }
-
         function testGetId()
         {
             $brand = "Converse";
             $price = "34";
             $test_shoe = new Shoe($brand, $price);
             $test_shoe->save();
-
             $result = $test_shoe->getId();
-
             $this->assertTrue(is_numeric($result));
         }
-
         function testSave()
         {
             $brand = "Kiks";
             $price = "78";
             $test_shoe = new Shoe($brand, $price);
             $test_shoe->save();
-
             $executed = $test_shoe->save();
-
             $this->assertTrue($executed, "Brand not successfully saved to database");
         }
-
         function testGetAll()
         {
             $brand = "Flippers";
@@ -107,12 +85,9 @@
             $test_shoe->save();
             $test_shoe_2 = new Shoe($brand_2, $price_2);
             $test_shoe_2->save();
-
             $result = Shoe::getAll();
-
             $this->assertEquals([$test_shoe, $test_shoe_2], $result);
         }
-
         function testDeleteAll()
         {
             $brand = "Flippers";
@@ -123,13 +98,10 @@
             $test_shoe->save();
             $test_shoe_2 = new Shoe($brand_2, $price_2);
             $test_shoe_2->save();
-
             Shoe::deleteAll();
-
             $result = Shoe::getAll();
             $this->assertEquals([], $result);
         }
-
         function testFind()
         {
             $brand = "Grrrkiks";
@@ -140,65 +112,59 @@
             $test_shoe->save();
             $test_shoe_2 = new Shoe($brand_2, $price_2);
             $test_shoe_2->save();
-
             $result = Shoe::find($test_shoe->getId());
-
             $this->assertEquals($test_shoe, $result);
         }
-
         function testUpdate()
         {
             $brand = "Gym";
             $price = "101";
             $test_brand = new Shoe($brand, $price);
             $test_brand->save();
-
             $new_brand = "Sneaks";
-
             $test_brand->update($new_brand);
-
             $this->assertEquals("Sneaks", $test_brand->getBrand());
         }
-
-        // function testAddStore()
-        // {
-        //     $store = "New Seasons";
-        //     $test_store = new Store($name);
-        //     $test_store->save();
-        //
-        //     $brand = "ana tomic";
-        //     $price = "88";
-        //     $test_brand = new Shoe($brand, $price);
-        //     $test_brand->save();
-        //
-        //     $test_brand->addStore($test_store);
-        //
-        //     $this->assertEquals($test_brand->getStores(), [$test_store]);
-        // }
-        //
-        // function testGetStores()
-        // {
-        //     $store = "Boblob";
-        //     $test_store = new Store($store);
-        //     $test_store->save();
-        //
-        //     $store2 = "Lora";
-        //     $test_store2 = new Store($store2);
-        //     $test_store2->save();
-        //
-        //     $brand = "anatomy";
-        //     $price = "888";
-        //     $test_brand = new Shoe($brand, $price);
-        //     $test_brand->save();
-        //
-        //     $test_brand->addStore($test_store);
-        //     $test_brand->addStore($test_store2);
-        //
-        //     $this->assertEquals($test_brand->getStores(), [$test_store, $test_store2]);
-        // }
-
-
-
-
     }
-    ?>
+    // function testAddStore()
+    // {
+    //     $store = "New Seasons";
+    //     $test_store = new Store($name);
+    //     $test_store->save();
+    //
+    //     $brand = "ana tomic";
+    //     $price = "88";
+    //     $test_brand = new Shoe($brand, $price);
+    //     $test_brand->save();
+    //
+    //     $test_brand->addStore($test_store);
+    //
+    //     $this->assertEquals($test_brand->getStores(), [$test_store]);
+    // }
+    //
+    // function testGetStores()
+    // {
+    //     $store = "Boblob";
+    //     $test_store = new Store($store);
+    //     $test_store->save();
+    //
+    //     $store2 = "Lora";
+    //     $test_store2 = new Store($store2);
+    //     $test_store2->save();
+    //
+    //     $brand = "anatomy";
+    //     $price = "888";
+    //     $test_brand = new Shoe($brand, $price);
+    //     $test_brand->save();
+    //
+    //     $test_brand->addStore($test_store);
+    //     $test_brand->addStore($test_store2);
+    //
+    //     $this->assertEquals($test_brand->getStores(), [$test_store, $test_store2]);
+    // }
+
+
+
+
+
+?>

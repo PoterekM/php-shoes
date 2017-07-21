4d71057f89eab67687b8f06daf874177737dcda3
Michelle Poterek <poterekm@gmail.com>
2017-07-21 15:07:46 -0700

    push before restarting- error message of Call to a member function run() on integer in /Users/Guest/Desktop/php-shoe-store/web/index.php on line 3

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Shoe.php";
+    require_once __DIR__."/../src/Store.php";
+
+
+    $server = 'mysql:host=localhost:8889;dbname=shoe_store';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // use Symfony\Component\HttpFoundation\Request;
+    // Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'shoes' => Shoe::getAll()));
+    });
+
+?>

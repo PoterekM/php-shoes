02aba7988dfc115c8129ad819d5979eec28634b6
Michelle Poterek <poterekm@gmail.com>
2017-07-23 22:03:50 -0700

    add patch to be able to modify store and create twig page

--- app/app.php
+++ app/app.php
@@ -23,38 +23,45 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-//
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'shoes' => Shoe::getAll()));
     });
 
-//
+
     $app->get("/stores", function() use ($app) {
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
-//
+
     $app->post("/stores", function() use ($app) {
         $location = $_POST['store'];
         $store = new Store($location);
         $store->save();
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
-//
+
     $app->get("/stores_empty", function() use ($app) {
         Store::deleteAll();
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
-    ///not sure if get is appropriate here in the stores empty
+
 
     $app->get("store/{id}", function($id) use ($app) {
         $store = Store::find($id);
         return $app['twig']->render('store.html.twig', array('store' => $store, 'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
     });
 
-    // $app->patch("/store/{id}", function ($id) use ($app) {
-    //     $store = Store::find($id);
-    //     return $app['twig']->render('store.html.twig', array('store' => $store, 'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
-    // });
+    $app->patch("/store/{id}", function ($id) use ($app) {
+        $store = Store::find($id);
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
+    });
+
+    $app->get("store/{id}/modify", function($id) use ($app) {
+        $location = $_POST['store'];
+        $store = Store::find($id);
+
+        return $app['twig']->render('store.html.twig', array('store' => $location, 'shoes'-> $store->getShoes(), 'all_shoes' => Shoe::getAll()));
+    });
 
     $app->post("/add_shoes", function() use ($app) {
         $store = Store::find($_POST['store_id']);
@@ -94,27 +101,6 @@
         return $app['twig']->render('shoe.html.twig', array('shoe' => $shoe, 'shoes' => Shoe::getAll(), 'stores' => $shoe->getStores(), 'all_stores' => Store::getAll()));
     });
 
-    //     'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
-    // });
-
-
-
-    // $app->post("store/{id}", function() use ($app) {
-    //     $store = $_POST['store'];
-    //     return $app['twig']->render('store.html.twig', array('stores' => Store::getAll(), 'store' => $store));
-    // });
-
-    // $app->post("/add_shoe")
-
-
-
-    // $app->post("/stores", function() use ($app) {
-    //     $brand = $_POST['brand'];
-    //     $price = $_POST['price'];
-    //     $shoe = new Shoe($brand, $price);
-    //     $shoe->save();
-    //     return $app['twig']->render('stores.html.twig', array('stores' => $store));
-    // });
 
 
     return $app;
